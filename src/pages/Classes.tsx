073cdd3717c5cd10c432e5867c9526833f6adbe4
Bruna Maria <bruna.mariap@outlook.com>
2023-12-01 15:43:18 -0300

    Adicionado modal para criar/editar/excluir turma

--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TCell from "../components/TCell";
 import THeader from "../components/THeader";
 import TRow from "../components/TRow";
@@ -7,12 +7,154 @@ import TActions from "../components/TActions";
 import Button from "../components/Button";
 import { IoMdAdd } from "react-icons/io";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import Modal from "../components/Modal";
+import { ControlledSelect } from "../components/Select";
+
+interface ClassData {
+  id: string;
+  referencePeriod: number;
+  shift: string;
+  courseId: string;
+  diaryId: string;
+}
+
+interface CreateClassData {
+  referencePeriod: number;
+  shift: string;
+  courseId: string;
+  diaryId: string;
+}
+
 const Classes: React.FC = () => {
+  const schema = yup.object().shape({
+    referencePeriod: yup.number().required("Campo obrigatório"),
+    shift: yup.string().required("Campo obrigatório"),
+    courseId: yup.string().required("Campo obrigatório"),
+    diaryId: yup.string().required("Campo obrigatório"),
+  });
+
+  const { control, handleSubmit, reset, watch } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const [showCreateClassModal, setShowCreateClassModal] = useState(false);
+  const [showDeleteClassModal, setShowDeleteClassModal] = useState(false);
+
+  const [classToRemove, setClassToRemove] = useState("");
+
+  const toggleCreateClassModal = () => {
+    reset({});
+    setShowCreateClassModal((visible) => !visible);
+  };
+
+  const toggleDeleteClassModal = () =>
+    setShowDeleteClassModal((visible) => !visible);
+
+  const handleEditClass = async (schoolClass: ClassData) => {
+    reset(schoolClass);
+    setShowCreateClassModal(true);
+  };
+
+  const handleDeleteClass = (classId: string) => {
+    setClassToRemove(classId);
+    toggleDeleteClassModal();
+  };
+
+  const onSubmitClass = async (data: CreateClassData) => {
+    console.log("result", data);
+    toggleCreateClassModal();
+  };
+
+  const onDeleteClass = async () => {
+    console.log("turma para remover", classToRemove);
+    toggleDeleteClassModal();
+  };
   return (
     <>
+      {showDeleteClassModal && (
+        <Modal
+          title="Tem certeza?"
+          description="Deseja excluir esta turma permanentemente?"
+          onClose={() => toggleDeleteClassModal()}
+          onConfirm={() => onDeleteClass()}
+        />
+      )}
+      {showCreateClassModal && (
+        <Modal
+          title="Criar nova turma"
+          description="Preencha todos os dados para criar uma nova turma"
+          onClose={() => toggleCreateClassModal()}
+          onConfirm={() => handleSubmit(onSubmitClass)}
+        >
+          <ControlledSelect
+            control={control}
+            name="courseId"
+            label="Curso"
+            placeholder="Selecione um curso"
+            options={[
+              {
+                label: "Análise e Desenvolvimento de Sistemas",
+                value: "1"
+              }
+            ]}
+          />
+          <ControlledSelect
+            control={control}
+            name="diaryId"
+            label="Período letivo"
+            placeholder="Selecione um período letivo"
+            options={[
+              {
+                label: "2023.2",
+                value: "1"
+              }
+            ]}
+          />
+          <ControlledSelect
+            control={control}
+            name="referencePeriod"
+            label="Período de referência"
+            placeholder="Selecione um período de referência"
+            disabled={!watch("courseId")}
+            options={[
+              {
+                label: "1",
+                value: "1"
+              },
+              {
+                label: "2",
+                value: "2"
+              },
+            ]}
+          />
+          <ControlledSelect
+            control={control}
+            name="shift"
+            label="Turno"
+            disabled={!watch("courseId")}
+            options={[
+              {
+                label: "Manhã",
+                value: "1",
+              },
+              {
+                label: "Tarde",
+                value: "1",
+              },
+              {
+                label: "Noite",
+                value: "1",
+              },
+            ]}
+          />
+        </Modal>
+      )}
       <section className="my-3 flex flex-row justify-end">
         <Button
-          onClick={() => {}}
+          onClick={() => toggleCreateClassModal()}
           className="flex flex-row items-center gap-2 py-3"
         >
           <IoMdAdd className="text-xl  text-white" />
@@ -38,17 +180,17 @@ const Classes: React.FC = () => {
             <TCell>2023.1</TCell>
             <TCell>7</TCell>
             <TCell className={"text-primary"}>
-              <TActions />
-            </TCell>
-          </TRow>
-          <TRow>
-            <TCell contrast>ADS6V</TCell>
-            <TCell>Análise e Desenvolvimento de Sistemas</TCell>
-            <TCell>Vespertino</TCell>
-            <TCell>2023.1</TCell>
-            <TCell>7</TCell>
-            <TCell className={"text-primary"}>
-              <TActions />
+              <TActions
+                showList={false}
+                onEditClick={() => handleEditClass({
+                  id: "1",
+                  referencePeriod: 2,
+                  shift: "Manhã",
+                  diaryId: "1",
+                  courseId: "1"
+                })}
+                onRemoveClick={() => handleDeleteClass("1")}
+              />
             </TCell>
           </TRow>
         </tbody>
